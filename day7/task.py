7c82bea668be5a872056b0d20c18a25058ecb705
Gerald <madgerald2009@gmail.com>
2025-11-20 21:05:49 +0100

    Gerald Assg

--- day7/task.py
+++ day7/task.py
@@ -5,3 +5,22 @@ multiply(a, b): returns the product of a and b
 power(a, b): returns a raised to the power of b
 subtract(a, b): returns the difference of a and b
 """
+
+def add(a, b):
+  return a + b
+
+def multiply(a, b):
+  return a * b
+
+def power(a, b):
+  return a ** b
+
+def subtract(a, b):
+  return a - b
+
+input_values = input("Enter two numbers separated by space: ")
+a, b = map(int, input_values.split())
+print(f"The addition =", add(a, b))        
+print(f"The multiplcation =", multiply(a, b))   
+print(f"The raised to power =", power(a, b))      
+print(f"The subtraction is =", subtract(a, b))  
